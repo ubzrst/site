8971e69c18a3afd331450d6090fb892b8a97831a
ubzrst <steveoncaffeine@gmail.com>
2026-03-25 18:47:23 -0400

    add mermaid support via astro-mermaid integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import mermaid from "astro-mermaid";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [
+        mermaid({ theme: 'default', autoTheme: true })
+    ],
+});
